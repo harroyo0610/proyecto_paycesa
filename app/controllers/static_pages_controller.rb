a05e8587402c039e614748df9c46870013a9b932
harroyo0610 <humberto-_-lzc@hotmail.com>
2017-12-12 10:31:35 -0600

    error de login

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,17 @@
 class StaticPagesController < ApplicationController
   def home
+  	if (user_id = session[:user_id])
+			@current_user ||= User.find_by(id: session[:user_id])	
+		elsif (user_id = cookies.signed[:user_id])
+			user = User.find_by(id: user_id)
+			if user && user.authenticated?(cookies[:remember_token])
+				log_in user
+				@current_user = user
+			end
+		end
+  	if @current_user.nil? 
+		redirect_to login_path
+	else 
+	end 		
   end
 end
